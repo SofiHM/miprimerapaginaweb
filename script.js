3e4fb552d60a7fc307423127482c17028cda88c7
Sofia Heredia <sofiaherediamancuso@gmail.com>
2023-09-14 17:55:02 -0300

    Segundo Commit

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ form.addEventListener('submit', function (event) {
 });
 document.querySelector("button.button-menu-toggle")
   .addEventListener("click", function () {
-    document.querySelector(".nav-links").
-      classList.toggle("nav-links-responsive")
-  })
+    document.querySelector(".nav-links")
+      classList.toggle("nav-links-responsive");
+  });
+  
